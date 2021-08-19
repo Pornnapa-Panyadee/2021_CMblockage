e1f2004b7f7ccffe3ed286c293019cd518c04579
pcsishun <pcsishun@Gmail.com>
2021-08-19 21:00:26 +0700

    update redict when register redict to /

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -31,7 +31,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/blocker';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
