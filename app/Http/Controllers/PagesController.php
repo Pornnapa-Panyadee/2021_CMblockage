7888283b1446c31773bd5207ec3aefa6f7b30237
pornnapa <pornnapa200@gmail.com>
2021-09-01 12:44:52 +0700

    test proj id

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -374,7 +374,7 @@ class PagesController extends Controller
       $solution_id=Solution::where('sol_id',$obj[0]->sol_id)->get();
       $project_id=Project::where('proj_id',$solution_id[0]->proj_id)->get();
 
-      dd($blk_project);
+      // dd($uid);
       // dd($project_id);
       if($project_id[0]->proj_status=="plan"){
         $proj =[
@@ -421,7 +421,7 @@ class PagesController extends Controller
         'blk_problem_detail' => $ProblemDetail,
         'uid' => $uid,
         'blk_damage_level' => $blk_damage_level,
-        'blk_project' => $blk_project[0],   
+        // 'blk_project' => $blk_project[0],   
         'solution'=>$solution_id ,
         'project'=>$proj ,
         'len_prob_value'=>$len_prob_value,
