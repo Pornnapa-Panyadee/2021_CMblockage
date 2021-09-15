b738a5b4ad5573fbbf1f4a2db2e7d871ca09646b
pcsishun <pcsishun@Gmail.com>
2021-09-15 19:32:19 +0700

    Change images info 1.4.2 -> 1.4.3 and 1.4.3 -> 1.4.2

--- app/Http/Controllers/BlockagesController.php
+++ app/Http/Controllers/BlockagesController.php
@@ -137,7 +137,7 @@ class BlockagesController extends Controller
         ->select(DB::raw("blockages.blk_code, blockages.blk_id, 
         JSON_EXTRACT(ST_AsGeoJSON(blk_start_location),'$.coordinates[0]') as latitude_start, 
         JSON_EXTRACT(ST_AsGeoJSON(blk_start_location), '$.coordinates[1]')as longitude_start, 
-        concat(blk_location_id,' ', blk_village,' ',blk_tumbol,' ',blk_province) as location,
+        concat(' ', blk_village,' ',blk_tumbol,' ',blk_province) as location,
         sqrt( ((JSON_EXTRACT(ST_AsGeoJSON(blk_start_location), '$.coordinates[1]') - $longitude_cast) * (JSON_EXTRACT(ST_AsGeoJSON(blk_start_location), '$.coordinates[1]') - $longitude_cast)) + ((JSON_EXTRACT(ST_AsGeoJSON(blk_start_location),'$.coordinates[0]') -  $latitude_cast) * (JSON_EXTRACT(ST_AsGeoJSON(blk_start_location),'$.coordinates[0]') -  $latitude_cast)) ) as distance"
         ))
         ->join('blockages', 'blockage_locations.blk_location_id', '=', 'blockages.blk_location_id')
